563d38163df86654790eb9dede12ce21f775a6c0
본인 GitHub 아이디[출처] <qead753951@naver.com>
2019-05-06 02:15:39 +0900

    modifedMoveMouseCamera

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+
+    void Update()
+    {
+        Vector3 tempV = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        tempV.z = 0f;
+        this.transform.position = tempV;
+
+    }
+    /*void OnMouseOver()
+    {
+        Debug.Log("마우스");
+    }*/
+        void OnTriggerStay2D(Collider2D other)
+    {   //if(other.transform.tag == "UI")      
+            //CameraMove.Instance.StopCamera(other);           
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //if (other.transform.tag == "UI")
+            //CameraMove.Instance.MoveCamera();
+    }
+}
