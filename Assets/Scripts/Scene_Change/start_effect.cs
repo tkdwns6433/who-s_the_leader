de4185ecd33c0ec1519e7e8620daf1f6660a76c7
LeeKangW <48194683+LeeKangW@users.noreply.github.com>
2019-05-06 15:31:53 +0900

    Scene Changed

---
+++ Assets/Scripts/Scene_Change/start_effect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class start_effect : MonoBehaviour
+{
+    public Image fade;
+    float fades = 1f;
+    float time = 0;
+    private void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (fades > 0f)
+        {
+            fade.color = new Color(0, 0, 0, fades);
+            fades -= 0.008f;
+           
+        }
+    }
+}
